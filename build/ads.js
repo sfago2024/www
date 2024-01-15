d73e9d29542a95be8e0e1cee7fb679c3718394cb
Colin Chan <colin+git@lumeh.org>
2024-01-15 15:45:59 -0800

    Implement ads

---
+++ build/ads.js
@@ -0,0 +1,15 @@
+function displayAdAndLogPageview() {
+  const ads = [
+    {name: 'sfago2024-placeholder', extension: 'png', url: 'https://www.sfago2024.org/advertise-exhibit/#maury-a-castro'},
+    {name: 'barenreiter', extension: 'jpg', url: 'https://www.barenreiter.us/products/bach-the-complete-organ-works-barenreiter?_pos=1&_psq=Bach+Organ+Works&_ss=e&_v=1.0'},
+    {name: 'barenreiter', extension: 'jpg', url: 'https://www.barenreiter.us/products/bach-the-complete-organ-works-barenreiter?_pos=1&_psq=Bach+Organ+Works&_ss=e&_v=1.0'},
+  ];
+  let ad = ads[Math.floor(Math.random() * ads.length)];
+  document.querySelectorAll(".ad.desktop img").forEach((img) => { img.src = `/ads/${ad.name}-desktop.${ad.extension}`; });
+  document.querySelectorAll(".ad.mobile img").forEach((img) => { img.src = `/ads/${ad.name}-mobile.${ad.extension}`; });
+  document.querySelectorAll(".ad a").forEach((a) => { a.href = ad.url; });
+
+  plausible('pageview', {props: {ad_displayed: ad.name}});
+}
+
+document.addEventListener("DOMContentLoaded", displayAdAndLogPageview);
